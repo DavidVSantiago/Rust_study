5210149af9acd23e850e0620b8933d42f5f5ee6d
DavidVSantiago <david.valente.santiago@gmail.com>
2025-10-05 15:12:34 -0300

    update

---
+++ 01-hello/hello_cargo/target/debug/deps/hello_cargo-f3bbb2b9d7a58c18.d
@@ -0,0 +1,5 @@
+/home/lubuntu/Documentos/_CODES/Rust_study/01-hello/hello_cargo/target/debug/deps/hello_cargo-f3bbb2b9d7a58c18.d: src/main.rs
+
+/home/lubuntu/Documentos/_CODES/Rust_study/01-hello/hello_cargo/target/debug/deps/hello_cargo-f3bbb2b9d7a58c18: src/main.rs
+
+src/main.rs:
---
+++ 01-hello/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/lubuntu/Documentos/_CODES/Rust_study/01-hello/hello_cargo/target/debug/hello_cargo: /home/lubuntu/Documentos/_CODES/Rust_study/01-hello/hello_cargo/src/main.rs
